eb789ba4e4600528d732a96a0937c778eeaa18d0
cristinacarrascosa <cctausias@gmail.com>
2022-12-16 12:07:17 +0100

    las flechas se eliminan cuando carga

--- js/app.js
+++ js/app.js
@@ -2,22 +2,27 @@ let aparece = document.querySelectorAll('.aparece');
 //console.log(aparece);
 let imagen = document.querySelectorAll('.entrada__imagen');
 //console.log(imagen);
-let flecha = document.querySelectorAll('.arrowSliding');
-console.log(flecha);
+let flecha = document.querySelector('#arrowAnim');
+//console.log(flecha);
 
 function mostrarScroll() {
     let scrollTop = document.documentElement.scrollTop;
-    
+    let alturaAnimado;
   for (var i=0; i < aparece.length; i++) {
-    let alturaAnimado = aparece[i].offsetTop;
+    alturaAnimado = aparece[i].offsetTop;
     if (alturaAnimado - 700 < scrollTop) {
       aparece[i].style.opacity = 1;
       aparece[i].classList.add('mostrarArriba');
      
-      
       //console.log(imagen[i]);
-      
     }
+    
+  }
+
+  if (alturaAnimado - 700 < scrollTop) {
+    flecha.remove();
+    // console.log(flecha);
+    // console.log('eliminado');
   }
 }
 
